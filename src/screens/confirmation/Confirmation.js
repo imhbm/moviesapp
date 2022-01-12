ad3765f7f34645516c25f93a0f7a379cca2a37d8
DevHimu <himandribhusan@gmail.com>
2022-01-13 00:06:54 +0530

    added screens details confirmation home

--- src/screens/confirmation/Confirmation.js
+++ src/screens/confirmation/Confirmation.js
@@ -1,268 +1,261 @@
-import React, { useState, useEffect } from "react";
-import Header from "../../common/header/Header";
-import "./Confirmation.css";
-import Typography from "@material-ui/core/Typography";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import FormControl from "@material-ui/core/FormControl";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Snackbar from "@material-ui/core/Snackbar";
-import IconButton from "@material-ui/core/IconButton";
-import CloseIcon from "@material-ui/icons/Close";
-import { withStyles } from "@material-ui/core/styles";
-import PropTypes from "prop-types";
-import CheckCircleIcon from "@material-ui/icons/CheckCircle";
-import green from "@material-ui/core/colors/green";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import Header from '../../common/header/Header';
+import './Confirmation.css';
+import Typography from '@material-ui/core/Typography';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import green from '@material-ui/core/colors/green';
+import { Link } from 'react-router-dom';
 
-const styles = (theme) => ({
+const styles = theme => ({
   close: {
     width: theme.spacing.unit * 4,
     height: theme.spacing.unit * 4,
   },
   success: {
     color: green[600],
-  },
+  }
 });
 
-const Confirmation = (props) => {
-  const [open, setOpen] = useState(false);
-  const [bookingId, setBookingId] = useState("");
-  const [couponCode, setCouponCode] = useState("");
-  const [totalPrice, setTotalPrice] = useState(0);
-  const [originalTotalPrice, setOriginalTotalPrice] = useState(0);
+class Confirmation extends Component {
 
-  useEffect(() => {
-    const price =
-      parseInt(props.location.bookingSummary.unitPrice, 10) *
-      parseInt(props.location.bookingSummary.tickets, 10);
-    setTotalPrice(price);
-    setOriginalTotalPrice(price);
-  }, []);
+  constructor() {
+    super();
 
-  const confirmBookingHandler = () => {
+    this.state = {
+      open: false,
+      bookingId: "",
+      couponCode: "",
+      totalPrice: 0,
+      originalTotalPrice: 0
+    }
+  }
+
+  componentDidMount() {
+    let currentState = this.state;
+    currentState.totalPrice = currentState.originalTotalPrice = parseInt(this.props.location.bookingSummary.unitPrice, 10) * parseInt(this.props.location.bookingSummary.tickets.length, 10);
+    this.setState({ state: currentState });
+  }
+
+  confirmBookingHandler = () => {
     let data = JSON.stringify({
-      coupon_code: couponCode,
-      show_id: props.location.bookingSummary.showId,
-      tickets: [props.location.bookingSummary.tickets.toString()],
+      "customerUuid": sessionStorage.getItem('uuid'),
+      "bookingRequest": {
+        "coupon_code": this.state.couponCode,
+        "show_id": this.props.location.bookingSummary.showId,
+        "tickets": [
+          this.props.location.bookingSummary.tickets.toString()
+        ]
+      }
     });
 
-    fetch(props.baseUrl + "bookings", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Cache-Control": "no-cache",
-        Authorization: "Bearer " + sessionStorage.getItem("access-token"),
-      },
-      body: data,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setBookingId(data.reference_number);
-      });
+    let that = this;
+    let xhr = new XMLHttpRequest();
 
-    setOpen(true);
-  };
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        that.setState({ bookingId: JSON.parse(this.responseText).reference_number });
+      }
+    });
 
-  const snackBarCloseHandler = () => {
-    props.history.push("/");
-  };
+    xhr.open("POST", this.props.baseUrl + "bookings");
+    xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem('access-token'));
+    xhr.setRequestHeader("Cache-Control", "no-cache");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(data);
 
-  const couponCodeChangeHandler = (e) => {
-    setCouponCode(e.target.value);
-  };
+    this.setState({ open: true });
+  }
 
-  const couponApplyHandler = () => {
-    fetch(props.baseUrl + "movies/" + props.match.params.id, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Cache-Control": "no-cache",
-        Authorization: "Bearer " + sessionStorage.getItem("access-token"),
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        let discountValue = data.value;
+  snackBarCloseHandler = () => {
+    this.props.history.push("/");
+  }
+
+  couponCodeChangeHandler = (e) => {
+    this.setState({ couponCode: e.target.value });
+  }
+
+  couponApplyHandler = () => {
+    console.log(this.state.couponCode);
+    let that = this;
+    let data = null;
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        let currentState = that.state;
+
+        let discountValue = JSON.parse(this.responseText).value;
         if (discountValue !== undefined && discountValue > 0) {
-          setTotalPrice(
-            originalTotalPrice - (originalTotalPrice * discountValue) / 100
-          );
+          currentState.totalPrice = that.state.originalTotalPrice - ((that.state.originalTotalPrice * discountValue) / 100);
+          that.setState({ currentState });
         } else {
-          setTotalPrice(originalTotalPrice);
+          currentState.totalPrice = that.state.originalTotalPrice;
+          that.setState({ currentState });
         }
-      });
-  };
+      }
+    });
 
-  const { classes } = props;
+    xhr.open("GET", this.props.baseUrl + "coupons/" + this.state.couponCode);
+    xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem('access-token'));
+    xhr.setRequestHeader("Cache-Control", "no-cache");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(data);
+  }
 
-  return (
-    <div className="Details">
-      <Header />
+  render() {
+    const { classes } = this.props;
 
-      <div className="confirmation marginTop16">
-        <div>
-          <Link to={"/bookshow/" + props.match.params.id}>
-            <Typography className="back">&#60; Back to Book Show</Typography>
-          </Link>
-          <br />
+    return (
+      <div className="Details">
+        <Header />
 
-          <Card className="cardStyle">
-            <CardContent>
-              <Typography variant="headline" component="h2">
-                SUMMARY
-              </Typography>
-              <br />
+        <div className="confirmation marginTop16">
+          <div>
+            <Link to={"/bookshow/" + this.props.match.params.id}>
+              <Typography className="back" >
+                &#60; Back to Book Show
+                </Typography>
+            </Link>
+            <br />
 
-              <div className="coupon-container">
-                <div className="confirmLeft">
-                  <Typography>Location:</Typography>
-                </div>
-                <div>
-                  <Typography>
-                    {props.location.bookingSummary.location}
-                  </Typography>
-                </div>
-              </div>
-              <br />
+            <Card className="cardStyle">
+              <CardContent>
+                <Typography variant="headline" component="h2">
+                  SUMMARY
+                </Typography>
+                <br />
 
-              <div className="coupon-container">
-                <div className="confirmLeft">
-                  <Typography>Theatre:</Typography>
+                <div className="coupon-container">
+                  <div className="confirmLeft">
+                    <Typography>Location:</Typography>
+                  </div>
+                  <div>
+                    <Typography>{this.props.location.bookingSummary.location}</Typography>
+                  </div>
                 </div>
-                <div>
-                  <Typography>
-                    {props.location.bookingSummary.theatre}
-                  </Typography>
-                </div>
-              </div>
-              <br />
+                <br />
 
-              <div className="coupon-container">
-                <div className="confirmLeft">
-                  <Typography>Language:</Typography>
+                <div className="coupon-container">
+                  <div className="confirmLeft">
+                    <Typography>Theatre:</Typography>
+                  </div>
+                  <div>
+                    <Typography>{this.props.location.bookingSummary.theatre}</Typography>
+                  </div>
                 </div>
-                <div>
-                  <Typography>
-                    {props.location.bookingSummary.language}
-                  </Typography>
-                </div>
-              </div>
-              <br />
+                <br />
 
-              <div className="coupon-container">
-                <div className="confirmLeft">
-                  <Typography>Show Date:</Typography>
+                <div className="coupon-container">
+                  <div className="confirmLeft">
+                    <Typography>Language:</Typography>
+                  </div>
+                  <div>
+                    <Typography>{this.props.location.bookingSummary.language}</Typography>
+                  </div>
                 </div>
-                <div>
-                  <Typography>
-                    {props.location.bookingSummary.showDate}
-                  </Typography>
-                </div>
-              </div>
-              <br />
+                <br />
 
-              <div className="coupon-container">
-                <div className="confirmLeft">
-                  <Typography>Tickets:</Typography>
+                <div className="coupon-container">
+                  <div className="confirmLeft">
+                    <Typography>Show Date:</Typography>
+                  </div>
+                  <div>
+                    <Typography>{this.props.location.bookingSummary.showDate}</Typography>
+                  </div>
                 </div>
-                <div>
-                  <Typography>
-                    {props.location.bookingSummary.tickets.toString()}
-                  </Typography>
-                </div>
-              </div>
-              <br />
+                <br />
 
-              <div className="coupon-container">
-                <div className="confirmLeft">
-                  <Typography>Unit Price:</Typography>
+                <div className="coupon-container">
+                  <div className="confirmLeft">
+                    <Typography>Tickets:</Typography>
+                  </div>
+                  <div>
+                    <Typography>{this.props.location.bookingSummary.tickets.toString()}</Typography>
+                  </div>
                 </div>
-                <div>
-                  <Typography>
-                    {props.location.bookingSummary.unitPrice}
-                  </Typography>
-                </div>
-              </div>
-              <br />
+                <br />
 
-              <div className="coupon-container">
-                <div>
-                  <FormControl className="formControl">
-                    <InputLabel htmlFor="coupon">
-                      <Typography>Coupon Code</Typography>
-                    </InputLabel>
-                    <Input id="coupon" onChange={couponCodeChangeHandler} />
-                  </FormControl>
+                <div className="coupon-container">
+                  <div className="confirmLeft">
+                    <Typography>Unit Price:</Typography>
+                  </div>
+                  <div>
+                    <Typography>{this.props.location.bookingSummary.unitPrice}</Typography>
+                  </div>
                 </div>
-                <div className="marginApply">
-                  <Button
-                    variant="contained"
-                    onClick={couponApplyHandler}
-                    color="primary"
-                  >
-                    Apply
+                <br />
+
+                <div className="coupon-container">
+                  <div>
+                    <FormControl className="formControl">
+                      <InputLabel htmlFor="coupon">
+                        <Typography>Coupon Code</Typography>
+                      </InputLabel>
+                      <Input id="coupon" onChange={this.couponCodeChangeHandler} />
+                    </FormControl>
+                  </div>
+                  <div className="marginApply">
+                    <Button variant="contained" onClick={this.couponApplyHandler.bind(this)} color="primary">Apply</Button>
+                  </div>
+                </div>
+                <br /><br />
+
+                <div className="coupon-container">
+                  <div className="confirmLeft">
+                    <span className="bold">Total Price:</span>
+                  </div>
+                  <div>{parseInt(this.state.totalPrice, 10)}</div>
+                </div>
+                <br />
+
+                <Button variant="contained" onClick={this.confirmBookingHandler} color="primary">
+                  Confirm Booking
                   </Button>
-                </div>
-              </div>
-              <br />
-              <br />
-
-              <div className="coupon-container">
-                <div className="confirmLeft">
-                  <span className="bold">Total Price:</span>
-                </div>
-                <div>{parseInt(totalPrice, 10)}</div>
-              </div>
-              <br />
-
-              <Button
-                variant="contained"
-                onClick={confirmBookingHandler}
-                color="primary"
-              >
-                Confirm Booking
-              </Button>
-            </CardContent>
-          </Card>
+              </CardContent>
+            </Card>
+          </div>
         </div>
-      </div>
 
-      <Snackbar
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "center",
-        }}
-        className="snackbar"
-        open={open}
-        onClose={snackBarCloseHandler}
-        message={
-          <span id="client-snackbar" className={classes.success}>
-            <div className="confirm">
-              <div>
-                <CheckCircleIcon />
-              </div>
-              <div className="message"> Booking Confirmed! {bookingId}</div>
-            </div>
-          </span>
-        }
-        action={[
-          <IconButton
-            key="close"
-            aria-label="Close"
-            color="inherit"
-            className={classes.close}
-            onClick={snackBarCloseHandler}
-          >
-            <CloseIcon />
-          </IconButton>,
-        ]}
-      />
-    </div>
-  );
-};
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+          className="snackbar"
+          open={this.state.open}
+          onClose={this.snackBarCloseHandler}
+          message={
+            <span id="client-snackbar" className={classes.success}>
+              <div className="confirm"><div><CheckCircleIcon /></div><div className="message"> Booking Confirmed!</div></div>
+            </span>
+          }
+          action={[
+            <IconButton
+              key="close"
+              aria-label="Close"
+              color="inherit"
+              className={classes.close}
+              onClick={this.snackBarCloseHandler}
+            >
+              <CloseIcon />
+            </IconButton>,
+          ]}
+        />
+      </div>
+    )
+  }
+}
 
 Confirmation.propTypes = {
   classes: PropTypes.object.isRequired,
